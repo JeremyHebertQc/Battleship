db2bac7cbbf30b148a77d79531f0984af68f2d1b
Vincent Gagnon <vincedeark@gmail.com>
2026-02-17 08:36:03 -0500

    elo wld

--- main.cpp
+++ main.cpp
@@ -7,6 +7,6 @@ int main() {
 	/* PLACEZ VOS TESTS ICI, MAIS RETIREZ-LES DU "MAIN()" */
 	/* UNE FOIS QUE VOTRE JEU DE BATTLESHIP EST COMPLÉTÉ. */
 	/******************************************************/
-
+	//test
 	return 0;
 }
