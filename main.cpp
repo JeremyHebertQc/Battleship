71361b37baf1e0ef994b4cc2d2debdc3c3680a1d
Jérémy Hébert <53281029+JeremyHebertQc@users.noreply.github.com>
2026-02-17 08:25:41 -0500

    Ajout des fichiers de départ

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "game.h"
+
+using namespace std;
+
+int main() {
+	/******************************************************/
+	/* PLACEZ VOS TESTS ICI, MAIS RETIREZ-LES DU "MAIN()" */
+	/* UNE FOIS QUE VOTRE JEU DE BATTLESHIP EST COMPLÉTÉ. */
+	/******************************************************/
+
+	return 0;
+}
