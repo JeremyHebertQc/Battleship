fddece190a3a6f0cef4cb346f4c08e8bdb913700
Vincent Gagnon <vincedeark@gmail.com>
2026-02-17 09:03:08 -0500

    test comment mi boi

--- main.cpp
+++ main.cpp
@@ -3,6 +3,12 @@ AUTEUR : Jérémy Hébert & Vincent Gagnon
 PROJET : Battleship - TP1
 DATE : 17 février 2026
 ====================================*/
+
+/******************************************************/
+/* PLACEZ VOS TESTS ICI, MAIS RETIREZ-LES DU "MAIN()" */
+/* UNE FOIS QUE VOTRE JEU DE BATTLESHIP EST COMPLÉTÉ. */
+/******************************************************/
+
 #include "game.h"
 
 int main() {
